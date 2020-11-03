5045373ac159c8d99a68a8c60ca1f8ad326dd85d
Yisi Liu <yisiliu@gmail.com>
2020-11-03 14:08:38 +0800

    Add on-demand mint

--- contracts/nft_test.sol
+++ contracts/nft_test.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract TestNFT is ERC721 {
+
+    uint8[] state_limits = [9,3,11,6,55,9,7,3,3,29,16,4,4,20,11,6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3];
+
     constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) public {
         _setBaseURI("https://mask-president-2020.s3.ap-east-1.amazonaws.com/");
-        for (uint i = 0; i < 10; i++){
-            _safeMint(msg.sender, i);
-        }
+    }
+
+    function mintStateToken(address claimer, uint8 state) public returns (uint256) {
+        uint256 _id = uint256(keccak256(abi.encodePacked(state, state_limits[state] - 1)));
+        _safeMint(claimer, _id);
+        state_limits[state] -= 1;
+        return _id;
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const TestNFT = artifacts.require("TestNFT");
+const web3 = require('Web3');
 
 contract("TestNFT", accounts => {
     beforeEach(async () =>{
@@ -8,12 +9,13 @@ contract("TestNFT", accounts => {
 
     it("Should return the correct base uri", async () => {
         const _baseURI = await nft.baseURI.call();
-        assert.equal(_baseURI, "https://mask.io/presidential_election/");
+        assert.equal(_baseURI, "https://mask-president-2020.s3.ap-east-1.amazonaws.com/");
     });
 
     it("Should transfer token id 0 to account 1", async () => {
-        const magic = await nft.safeTransferFrom.sendTransaction(accounts[0], accounts[1], 0);
-        const ownership = await nft.ownerOf(0);
+        const magic = await nft.mintStateToken.sendTransaction(accounts[1], 0);
+        const _id = web3.utils.soliditySha3(0, 8);
+        const ownership = await nft.ownerOf(_id);
         assert.equal(ownership, accounts[1]);
     });
 });
