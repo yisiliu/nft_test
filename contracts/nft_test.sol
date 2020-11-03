a7ecfb75b6b2db009b04f003846a77423df2ab87
Yisi Liu <yisiliu@gmail.com>
2020-11-03 16:07:40 +0800

    Add a few functions to the contract and update the tests

--- contracts/nft_test.sol
+++ contracts/nft_test.sol
@@ -4,16 +4,30 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract TestNFT is ERC721 {
 
-    uint8[] state_limits = [9,3,11,6,55,9,7,3,3,29,16,4,4,20,11,6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3];
+    uint8[] private state_limits = [9,3,11,6,55,9,7,3,3,29,16,4,4,20,11,6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3];
+    address private creator;
 
     constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) public {
         _setBaseURI("https://mask-president-2020.s3.ap-east-1.amazonaws.com/");
+        creator = msg.sender;
     }
 
     function mintStateToken(address claimer, uint8 state) public returns (uint256) {
+        require(msg.sender == creator, "Not authorized.");
+        require(state_limits[state] > 0, "Out of stock.");
         uint256 _id = uint256(keccak256(abi.encodePacked(state, state_limits[state] - 1)));
         _safeMint(claimer, _id);
         state_limits[state] -= 1;
         return _id;
+    }
+
+    function check_availability(uint8 state) public view returns (uint8) {
+        return state_limits[state];
+    }
+
+    function modify_limits(uint8 state, int8 delta) public {
+        require(msg.sender == creator, "Not authorized.");
+        require(int8(state_limits[state]) + delta > 0);        
+        state_limits[state] = uint8(int8(state_limits[state]) + delta);
     }
 }
--- test/test.js
+++ test/test.js
@@ -12,10 +12,22 @@ contract("TestNFT", accounts => {
         assert.equal(_baseURI, "https://mask-president-2020.s3.ap-east-1.amazonaws.com/");
     });
 
-    it("Should transfer token id 0 to account 1", async () => {
+    it("Should transfer a newly minted token to account 1", async () => {
         const magic = await nft.mintStateToken.sendTransaction(accounts[1], 0);
-        const _id = web3.utils.soliditySha3(0, 8);
+        const _id = web3.utils.soliditySha3({t: 'uint8', v: 0}, {t: 'uint8', v: 8});
         const ownership = await nft.ownerOf(_id);
         assert.equal(ownership, accounts[1]);
     });
+
+    it("Should return the remaining tokens for the state 0", async () => {
+        const remaining = await nft.check_availability(0);
+        assert.equal(remaining, 8);
+    });
+
+    it("Should reset the remaining tokens for the state 0", async () => {
+        await nft.modify_limits.sendTransaction(0, 1);
+        const remaining = await nft.check_availability(0);
+        assert.equal(remaining, 9);
+    });
+
 });
